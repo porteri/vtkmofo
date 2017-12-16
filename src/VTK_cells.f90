6b38f0302a73df6a20bff113e5be913491cb0f81
Ian Porter PC <Ian.Porter@nrc.gov>
2017-12-16 17:40:00 -0500

    Added all cell types
    Also updated unit testing

--- src/VTK_cells.f90
+++ src/VTK_cells.f90
@@ -7,16 +7,39 @@ MODULE vtk_cells
     !> Ian Porter
     !>@date
     !> 12/2/2017
+    !
+    ! The following cells are available:
+    !  1) vertex
+    !  2) poly_vertex
+    !  3) line
+    !  4) poly_line
+    !  5) triangle
+    !  6) triangle_strip
+    !  7) polygon
+    !  8) pixel
+    !  9) quad
+    ! 10) tetra
+    ! 11) voxel
+    ! 12) hexahedron
+    ! 13) wedge
+    ! 14) pyramid
+    ! 15) quadratic_edge
+    ! 16) quadratic_triangle
+    ! 17) quadratic_quad
+    ! 18) quadratic_tetra
+    ! 19) quadratic_hexahedron
+    !
 
     PRIVATE
-    PUBLIC :: vtkcell, vertex
+    PUBLIC :: vtkcell, vertex, poly_vertex, line, poly_line, triangle, triangle_strip, polygon, pixel, quad, tetra, voxel
+    PUBLIC :: hexahedron, wedge, pyramid, quadratic_edge, quadratic_triangle, quadratic_quad, quadratic_tetra, quadratic_hexahedron
 
     TYPE, ABSTRACT :: vtkcell
         INTEGER(i4k) :: n_points
         INTEGER(i4k) :: type
         INTEGER(i4k), DIMENSION(:), ALLOCATABLE :: points
     CONTAINS
-        PROCEDURE(abs_init),  DEFERRED, PUBLIC :: init
+        PROCEDURE(abs_init), DEFERRED, PRIVATE :: init
         PROCEDURE, PUBLIC :: setup => abs_setup
         PROCEDURE :: define => abs_define
         PROCEDURE, PUBLIC :: read   => abs_read
@@ -25,61 +48,433 @@ MODULE vtk_cells
 
     TYPE, EXTENDS(vtkcell) :: vertex
     CONTAINS
-        PROCEDURE :: init   => vertex_init
+        PROCEDURE :: init => vertex_init
     END TYPE vertex
+
+    TYPE, EXTENDS(vtkcell) :: poly_vertex
+    CONTAINS
+        PROCEDURE :: init => poly_vertex_init
+    END TYPE poly_vertex
+
+    TYPE, EXTENDS(vtkcell) :: line
+    CONTAINS
+        PROCEDURE :: init => line_init
+    END TYPE line
+
+    TYPE, EXTENDS(vtkcell) :: poly_line
+    CONTAINS
+        PROCEDURE :: init => poly_line_init
+    END TYPE poly_line
+
+    TYPE, EXTENDS(vtkcell) :: triangle
+    CONTAINS
+        PROCEDURE :: init => triangle_init
+    END TYPE triangle
+
+    TYPE, EXTENDS(vtkcell) :: triangle_strip
+    CONTAINS
+        PROCEDURE :: init => triangle_strip_init
+    END TYPE triangle_strip
+
+    TYPE, EXTENDS(vtkcell) :: polygon
+    CONTAINS
+        PROCEDURE :: init => polygon_init
+    END TYPE polygon
+
+    TYPE, EXTENDS(vtkcell) :: pixel
+    CONTAINS
+        PROCEDURE :: init => pixel_init
+    END TYPE pixel
+
+    TYPE, EXTENDS(vtkcell) :: quad
+    CONTAINS
+        PROCEDURE :: init => quad_init
+    END TYPE quad
+
+    TYPE, EXTENDS(vtkcell) :: tetra
+    CONTAINS
+        PROCEDURE :: init => tetra_init
+    END TYPE tetra
 
     TYPE, EXTENDS(vtkcell) :: voxel
     CONTAINS
-        PROCEDURE :: init   => voxel_init
+        PROCEDURE :: init => voxel_init
     END TYPE voxel
 
+    TYPE, EXTENDS(vtkcell) :: hexahedron
     CONTAINS
-        SUBROUTINE abs_init (me)
+        PROCEDURE :: init => hexahedron_init
+    END TYPE hexahedron
+
+    TYPE, EXTENDS(vtkcell) :: wedge
+    CONTAINS
+        PROCEDURE :: init => wedge_init
+    END TYPE wedge
+
+    TYPE, EXTENDS(vtkcell) :: pyramid
+    CONTAINS
+        PROCEDURE :: init => pyramid_init
+    END TYPE pyramid
+
+    TYPE, EXTENDS(vtkcell) :: quadratic_edge
+    CONTAINS
+        PROCEDURE :: init => quadratic_edge_init
+    END TYPE quadratic_edge
+
+    TYPE, EXTENDS(vtkcell) :: quadratic_triangle
+    CONTAINS
+        PROCEDURE :: init => quadratic_triangle_init
+    END TYPE quadratic_triangle
+
+    TYPE, EXTENDS(vtkcell) :: quadratic_quad
+    CONTAINS
+        PROCEDURE :: init => quadratic_quad_init
+    END TYPE quadratic_quad
+
+    TYPE, EXTENDS(vtkcell) :: quadratic_tetra
+    CONTAINS
+        PROCEDURE :: init => quadratic_tetra_init
+    END TYPE quadratic_tetra
+
+    TYPE, EXTENDS(vtkcell) :: quadratic_hexahedron
+    CONTAINS
+        PROCEDURE :: init => quadratic_hexahedron_init
+    END TYPE quadratic_hexahedron
+
+    CONTAINS
+        PURE SUBROUTINE abs_init (me, n)
         CLASS(vtkcell), INTENT(OUT) :: me
-        me%type = 0
+        INTEGER(i4k),   INTENT(IN) :: n
+        me%n_points = n
         END SUBROUTINE abs_init
 
-        SUBROUTINE vertex_init (me)
-        CLASS(vertex), INTENT(OUT) :: me
-        me%n_points = 1
-        me%type     = 1
-        END SUBROUTINE vertex_init
-
-        SUBROUTINE voxel_init (me)
-        CLASS(voxel), INTENT(OUT) :: me
-        me%n_points = 8
-        me%type     = 11
-        END SUBROUTINE voxel_init
-
         SUBROUTINE abs_setup (me, points)
-        CLASS(vtkcell), INTENT(INOUT) :: me
+        !>@brief
+        !> Sets up the cell information
+        CLASS(vtkcell), INTENT(OUT) :: me
         INTEGER(i4k), DIMENSION(:), INTENT(IN) :: points
+
+        CALL me%init(SIZE(points))     !! Initialize the cell
+
         me%points = points
+
         END SUBROUTINE abs_setup
 
         SUBROUTINE abs_define (me, points)
-        CLASS(vtkcell), INTENT(INOUT) :: me
+        CLASS(vtkcell), INTENT(OUT) :: me
         INTEGER(i4k), DIMENSION(:), INTENT(IN) :: points
 
-        IF (ALLOCATED(me%points)) DEALLOCATE(me%points)
         me%points = points
 
         END SUBROUTINE abs_define
 
         SUBROUTINE abs_read (me, unit)
+        USE Misc, ONLY : interpret_string, def_len
+        !>@brief
+        !> Subroutine performs the read for a cell
+        !>@author
+        !> Ian Porter, NRC
+        !>@date
+        !> 12/15/2017
         CLASS(vtkcell), INTENT(INOUT) :: me
-        INTEGER(i4k), INTENT(IN) :: unit
-        INTEGER(i4k) :: i
-        READ(unit,100) me%n_points, (me%points(i),i=1,me%n_points)
-100     FORMAT ((i0,' '),*(i0,' '))
+        INTEGER(i4k),   INTENT(IN)    :: unit
+        INTEGER(i4k)                  :: i, iostat
+        LOGICAL                       :: end_of_file
+        CHARACTER(LEN=def_len)        :: line
+        INTEGER(i4k), DIMENSION(:), ALLOCATABLE :: ints, dummy
+
+        ALLOCATE(me%points(0)); i = -1; end_of_file = .FALSE.
+
+        READ(unit,100,iostat=iostat) line
+        end_of_file = (iostat < 0)
+        IF (end_of_file) THEN
+            RETURN
+        ELSE
+            get_vals: DO
+                ALLOCATE(dummy(1:UBOUND(me%points,DIM=1)+1),source=0)
+                IF (i > 0) dummy(1:UBOUND(me%points,DIM=1)) = me%points
+                CALL MOVE_ALLOC(dummy, me%points)
+
+                i = i + 1
+                CALL interpret_string (line=line, datatype=(/ 'I' /), separator=' ', ints=ints)
+                IF (i == 0) THEN
+                    CALL me%init(ints(1))
+                    IF (ALLOCATED(me%points)) DEALLOCATE(me%points)
+                    ALLOCATE(me%points(0))
+                ELSE
+                    me%points(i) = ints(1)
+                END IF
+                IF (line == '') EXIT
+            END DO get_vals
+        END IF
+
+100     FORMAT((a))
         END SUBROUTINE abs_read
 
         SUBROUTINE abs_write (me, unit)
         CLASS(vtkcell), INTENT(IN) :: me
-        INTEGER(i4k), INTENT(IN) :: unit
+        INTEGER(i4k),   INTENT(IN) :: unit
         INTEGER(i4k) :: i
+
         WRITE(unit,100) me%n_points, (me%points(i),i=1,me%n_points)
 100     FORMAT ((i0,' '),*(i0,' '))
         END SUBROUTINE abs_write
 
+        PURE SUBROUTINE vertex_init (me, n)
+        !>@brief
+        !> Initializes a vertex cell
+        CLASS(vertex), INTENT(OUT) :: me
+        INTEGER(i4k),  INTENT(IN)  :: n
+
+        me%n_points = 1
+        me%type     = 1
+        IF (n /= me%n_points) THEN
+            ERROR STOP 'Incorrect number of points provide to initialize vertex'
+        END IF
+
+        END SUBROUTINE vertex_init
+
+        PURE SUBROUTINE poly_vertex_init (me, n)
+        !>@brief
+        !> Initializes a poly_vertex cell
+        CLASS(poly_vertex), INTENT(OUT) :: me
+        INTEGER(i4k),       INTENT(IN)  :: n
+
+        me%n_points = 7 !! May be n instead?
+        me%type     = 2
+        IF (n /= me%n_points) THEN
+            ERROR STOP 'Incorrect number of points provide to initialize poly_vertex'
+        END IF
+
+        END SUBROUTINE poly_vertex_init
+
+        PURE SUBROUTINE line_init (me, n)
+        !>@brief
+        !> Initializes a line cell
+        CLASS(line),  INTENT(OUT) :: me
+        INTEGER(i4k), INTENT(IN)  :: n
+
+        me%n_points = 2
+        me%type     = 3
+        IF (n /= me%n_points) THEN
+            ERROR STOP 'Incorrect number of points provide to initialize line'
+        END IF
+
+        END SUBROUTINE line_init
+
+        PURE SUBROUTINE poly_line_init (me, n)
+        !>@brief
+        !> Initializes a poly_line cell
+        CLASS(poly_line), INTENT(OUT) :: me
+        INTEGER(i4k),     INTENT(IN)  :: n
+
+        me%n_points = n
+        me%type     = 4
+
+        END SUBROUTINE poly_line_init
+
+        PURE SUBROUTINE triangle_init (me, n)
+        !>@brief
+        !> Initializes a triangle cell
+        CLASS(triangle), INTENT(OUT) :: me
+        INTEGER(i4k),    INTENT(IN)  :: n
+
+        me%n_points = 3
+        me%type     = 5
+        IF (n /= me%n_points) THEN
+            ERROR STOP 'Incorrect number of points provide to initialize triangle'
+        END IF
+
+        END SUBROUTINE triangle_init
+
+        PURE SUBROUTINE triangle_strip_init (me, n)
+        !>@brief
+        !> Initializes a triangle_strip cell
+        CLASS(triangle_strip), INTENT(OUT) :: me
+        INTEGER(i4k),          INTENT(IN)  :: n
+
+        me%n_points = n
+        me%type     = 6
+
+        END SUBROUTINE triangle_strip_init
+
+
+        PURE SUBROUTINE polygon_init (me, n)
+        !>@brief
+        !> Initializes a polygon cell
+        CLASS(polygon), INTENT(OUT) :: me
+        INTEGER(i4k),   INTENT(IN)  :: n
+
+        me%n_points = n
+        me%type     = 7
+
+        END SUBROUTINE polygon_init
+
+        PURE SUBROUTINE pixel_init (me, n)
+        !>@brief
+        !> Initializes a pixel cell
+        CLASS(pixel), INTENT(OUT) :: me
+        INTEGER(i4k), INTENT(IN)  :: n
+
+        me%n_points = 4
+        me%type     = 8
+        IF (n /= me%n_points) THEN
+            ERROR STOP 'Incorrect number of points provide to initialize pixel'
+        END IF
+
+        END SUBROUTINE pixel_init
+
+        PURE SUBROUTINE quad_init (me, n)
+        !>@brief
+        !> Initializes a quad cell
+        CLASS(quad),  INTENT(OUT) :: me
+        INTEGER(i4k), INTENT(IN)  :: n
+
+        me%n_points = 4
+        me%type     = 9
+        IF (n /= me%n_points) THEN
+            ERROR STOP 'Incorrect number of points provide to initialize quad'
+        END IF
+
+        END SUBROUTINE quad_init
+
+        PURE SUBROUTINE tetra_init (me, n)
+        !>@brief
+        !> Initializes a tetra cell
+        CLASS(tetra), INTENT(OUT) :: me
+        INTEGER(i4k), INTENT(IN)  :: n
+
+        me%n_points = 4
+        me%type     = 10
+        IF (n /= me%n_points) THEN
+            ERROR STOP 'Incorrect number of points provide to initialize tetra'
+        END IF
+
+        END SUBROUTINE tetra_init
+
+        PURE SUBROUTINE voxel_init (me, n)
+        !>@brief
+        !> Initializes a voxel cell
+        CLASS(voxel), INTENT(OUT) :: me
+        INTEGER(i4k), INTENT(IN)  :: n
+
+        me%n_points = 8
+        me%type     = 11
+        IF (n /= me%n_points) THEN
+            ERROR STOP 'Incorrect number of points provide to initialize voxel'
+        END IF
+
+        END SUBROUTINE voxel_init
+
+        PURE SUBROUTINE hexahedron_init (me, n)
+        !>@brief
+        !> Initializes a hexahedron cell
+        CLASS(hexahedron), INTENT(OUT) :: me
+        INTEGER(i4k),    INTENT(IN)  :: n
+
+        me%n_points = 8
+        me%type     = 12
+        IF (n /= me%n_points) THEN
+            ERROR STOP 'Incorrect number of points provide to initialize hexahedron'
+        END IF
+
+        END SUBROUTINE hexahedron_init
+
+        PURE SUBROUTINE wedge_init (me, n)
+        !>@brief
+        !> Initializes a wedge cell
+        CLASS(wedge), INTENT(OUT) :: me
+        INTEGER(i4k), INTENT(IN)  :: n
+
+        me%n_points = 6
+        me%type     = 13
+        IF (n /= me%n_points) THEN
+            ERROR STOP 'Incorrect number of points provide to initialize wedge'
+        END IF
+
+        END SUBROUTINE wedge_init
+
+        PURE SUBROUTINE pyramid_init (me, n)
+        !>@brief
+        !> Initializes a pyramid cell
+        CLASS(pyramid), INTENT(OUT) :: me
+        INTEGER(i4k),   INTENT(IN)  :: n
+
+        me%n_points = 5
+        me%type     = 14
+        IF (n /= me%n_points) THEN
+            ERROR STOP 'Incorrect number of points provide to initialize pyramid'
+        END IF
+
+        END SUBROUTINE pyramid_init
+
+        PURE SUBROUTINE quadratic_edge_init (me, n)
+        !>@brief
+        !> Initializes a quadratic_edge cell
+        CLASS(quadratic_edge), INTENT(OUT) :: me
+        INTEGER(i4k),          INTENT(IN)  :: n
+
+        me%n_points = 3
+        me%type     = 21
+        IF (n /= me%n_points) THEN
+            ERROR STOP 'Incorrect number of points provide to initialize quadratic_edge'
+        END IF
+
+        END SUBROUTINE quadratic_edge_init
+
+        PURE SUBROUTINE quadratic_triangle_init (me, n)
+        !>@brief
+        !> Initializes a quadratic_triangle cell
+        CLASS(quadratic_triangle), INTENT(OUT) :: me
+        INTEGER(i4k),              INTENT(IN)  :: n
+
+        me%n_points = 6
+        me%type     = 22
+        IF (n /= me%n_points) THEN
+            ERROR STOP 'Incorrect number of points provide to initialize quadratic_triangle'
+        END IF
+
+        END SUBROUTINE quadratic_triangle_init
+
+        PURE SUBROUTINE quadratic_quad_init (me, n)
+        !>@brief
+        !> Initializes a quadratic_quad cell
+        CLASS(quadratic_quad), INTENT(OUT) :: me
+        INTEGER(i4k),          INTENT(IN)  :: n
+
+        me%n_points = 8
+        me%type     = 23
+        IF (n /= me%n_points) THEN
+            ERROR STOP 'Incorrect number of points provide to initialize quadratic_quad'
+        END IF
+
+        END SUBROUTINE quadratic_quad_init
+
+        PURE SUBROUTINE quadratic_tetra_init (me, n)
+        !>@brief
+        !> Initializes a quadratic_tetra cell
+        CLASS(quadratic_tetra), INTENT(OUT) :: me
+        INTEGER(i4k),           INTENT(IN)  :: n
+
+        me%n_points = 10
+        me%type     = 24
+        IF (n /= me%n_points) THEN
+            ERROR STOP 'Incorrect number of points provide to initialize quadratic_tetra'
+        END IF
+
+        END SUBROUTINE quadratic_tetra_init
+
+        PURE SUBROUTINE quadratic_hexahedron_init (me, n)
+        !>@brief
+        !> Initializes a quadratic_hexahedron cell
+        CLASS(quadratic_hexahedron), INTENT(OUT) :: me
+        INTEGER(i4k),                INTENT(IN)  :: n
+
+        me%n_points = 20
+        me%type     = 25
+        IF (n /= me%n_points) THEN
+            ERROR STOP 'Incorrect number of points provide to initialize quadratic_hexahedron'
+        END IF
+
+        END SUBROUTINE quadratic_hexahedron_init
 END MODULE vtk_cells
---
+++ tests/unit/cells_unit.f90
@@ -0,0 +1,170 @@
+MODULE vtk_cells_unit_tests
+    USE Kinds
+    IMPLICIT NONE
+    !>@brief
+    !> Unit testing for cells
+    !>@author
+    !> Ian Porter
+    !>@date
+    !> 12/16/2017
+    PRIVATE
+    PUBLIC :: vtk_cells_unit
+! Generic information
+    INTEGER(i4k), PARAMETER :: n = 11
+    INTEGER(i4k),      DIMENSION(*), PARAMETER :: type       = &
+        & [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 21, 22, 23, 24, 25 ]
+    INTEGER(i4k),      DIMENSION(*), PARAMETER :: vtk_unit   = type
+    INTEGER(i4k),      DIMENSION(*), PARAMETER :: n_points   = &
+        & [ 1, 7, 2, n, 3, n, n, 4, 4,  4,  8,  8,  6,  5,  3,  6,  8, 10, 20 ]
+    INTEGER(i4k),      DIMENSION(*), PARAMETER :: point_vals = &
+        & [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ]
+    CHARACTER(LEN=25), DIMENSION(*), PARAMETER :: filename   = &
+        & [ 'vertex.vtk               ', &
+        &   'poly_vertex.vtk          ', &
+        &   'line.vtk                 ', &
+        &   'poly_line.vtk            ', &
+        &   'triangle.vtk             ', &
+        &   'triangle_strip.vtk       ', &
+        &   'polygon.vtk              ', &
+        &   'pixel.vtk                ', &
+        &   'quad.vtk                 ', &
+        &   'tetra.vtk                ', &
+        &   'voxel.vtk                ', &
+        &   'hexahedron.vtk           ', &
+        &   'wedge.vtk                ', &
+        &   'pyramid.vtk              ', &
+        &   'quadratic_edge.vtk       ', &
+        &   'quadratic_triangle.vtk   ', &
+        &   'quadratic_quad.vtk       ', &
+        &   'quadratic_tetra.vtk      ', &
+        &   'quadratic_hexahedron.vtk ' ]
+    CONTAINS
+        SUBROUTINE vtk_cells_unit (test_pass)
+        USE Kinds
+        USE vtk_cells, ONLY : vtkcell, vertex, poly_vertex, line, poly_line, triangle, triangle_strip, polygon, pixel, quad, &
+          &                   tetra, voxel, hexahedron, wedge, pyramid, quadratic_edge, quadratic_triangle, quadratic_quad,  &
+          &                   quadratic_tetra, quadratic_hexahedron
+        USE PassFail,  ONLY : Analyze
+        IMPLICIT NONE
+        !>@brief
+        !> Loops over each cell type, performs a write, then performs a read on a different cell
+        !> and compares the two to make sure they are identical
+        CLASS(vtkcell), ALLOCATABLE        :: vtk_cell_1, vtk_cell_2
+        INTEGER(i4k)                       :: i, j
+        LOGICAL, INTENT(OUT)               :: test_pass
+        LOGICAL, DIMENSION(SIZE(filename)) :: individual_tests_pass
+
+        DO i = 1, SIZE(filename,DIM=1)
+            IF (ALLOCATED(vtk_cell_1)) DEALLOCATE(vtk_cell_1)
+            IF (ALLOCATED(vtk_cell_2)) DEALLOCATE(vtk_cell_2)
+            SELECT CASE (type(i))
+            CASE (1)
+                !! Vertex
+                ALLOCATE(vertex::vtk_cell_1, vtk_cell_2)
+            CASE (2)
+                !! Poly_vertex
+                ALLOCATE(poly_vertex::vtk_cell_1, vtk_cell_2)
+            CASE (3)
+                !! Line
+                ALLOCATE(line::vtk_cell_1, vtk_cell_2)
+            CASE (4)
+                !! Poly_line
+                ALLOCATE(poly_line::vtk_cell_1, vtk_cell_2)
+            CASE (5)
+                !! Triangle
+                ALLOCATE(triangle::vtk_cell_1, vtk_cell_2)
+            CASE (6)
+                !! Triangle_strip
+                ALLOCATE(triangle_strip::vtk_cell_1, vtk_cell_2)
+            CASE (7)
+                !! Polygon
+                ALLOCATE(polygon::vtk_cell_1, vtk_cell_2)
+            CASE (8)
+                !! Pixel
+                ALLOCATE(pixel::vtk_cell_1, vtk_cell_2)
+            CASE (9)
+                !! Quad
+                ALLOCATE(quad::vtk_cell_1, vtk_cell_2)
+            CASE (10)
+                !! Tetra
+                ALLOCATE(tetra::vtk_cell_1, vtk_cell_2)
+            CASE (11)
+                !! Vine
+                ALLOCATE(voxel::vtk_cell_1, vtk_cell_2)
+            CASE (12)
+                !! Hexahedron
+                ALLOCATE(hexahedron::vtk_cell_1, vtk_cell_2)
+            CASE (13)
+                !! Wedge
+                ALLOCATE(wedge::vtk_cell_1, vtk_cell_2)
+            CASE (14)
+                !! pyramid
+                ALLOCATE(pyramid::vtk_cell_1, vtk_cell_2)
+            CASE (21)
+                !! Quadratic_edge
+                ALLOCATE(quadratic_edge::vtk_cell_1, vtk_cell_2)
+            CASE (22)
+                !! Quadratic_triangle
+                ALLOCATE(quadratic_triangle::vtk_cell_1, vtk_cell_2)
+            CASE (23)
+                !! Quadratic_quad
+                ALLOCATE(quadratic_quad::vtk_cell_1, vtk_cell_2)
+            CASE (24)
+                !! Quadratic_tetra
+                ALLOCATE(quadratic_tetra::vtk_cell_1, vtk_cell_2)
+            CASE (25)
+                !! Quadratic_hexahedron
+                ALLOCATE(quadratic_hexahedron::vtk_cell_1, vtk_cell_2)
+            END SELECT
+
+            !! Data type is generated from the defined values above
+            CALL vtk_cell_1%setup(points=point_vals(1:n_points(i)))
+            OPEN (unit=vtk_unit(i), file=filename(i), form = 'formatted')
+            CALL vtk_cell_1%write(vtk_unit(i))
+            CLOSE(unit=vtk_unit(i))
+
+            !! Data type is generated from the read
+            OPEN (unit=vtk_unit(i), file=filename(i), status='old', form = 'formatted')
+            CALL vtk_cell_2%read(vtk_unit(i))
+            CLOSE(unit=vtk_unit(i))
+
+            individual_tests_pass(i) = .TRUE.
+            IF      (vtk_cell_1%n_points     /= vtk_cell_2%n_points) THEN
+                individual_tests_pass(i) = .FALSE.
+            ELSE IF (vtk_cell_1%type         /= vtk_cell_2%type    ) THEN
+                individual_tests_pass(i) = .FALSE.
+            ELSE IF (SIZE(vtk_cell_1%points) /= SIZE(vtk_cell_2%points)) THEN
+                individual_tests_pass(i) = .FALSE.
+            ELSE
+                DO j = 1, SIZE(vtk_cell_1%points)
+                    IF (vtk_cell_1%points(j) /= vtk_cell_2%points(j)) THEN
+                        individual_tests_pass(i) = .FALSE.
+                        EXIT
+                    END IF
+                END DO
+            END IF
+        END DO
+
+        !! Compare the read file and the written/read file to ensure both types are the same
+        test_pass = ALL(individual_tests_pass)
+
+        END SUBROUTINE vtk_cells_unit
+END MODULE vtk_cells_unit_tests
+
+PROGRAM vtk_cells_test
+    USE vtk_cells_unit_tests, ONLY : vtk_cells_unit
+    USE PassFail,             ONLY : all_tests_pass
+    IMPLICIT NONE
+    !>@brief
+    !> Driver testing subroutine for the cells information
+    !>@author
+    !> Ian Porter
+    !>@date
+    !> 12/15/2017
+    LOGICAL :: test_passes = .FALSE.
+
+    CALL vtk_cells_unit (test_passes)
+
+    IF (test_passes) CALL all_tests_pass()
+
+END PROGRAM vtk_cells_test
